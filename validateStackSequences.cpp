b87b674ef5502a4c0d4886d0c0f9950dc17e8a89
peng <2460819991@qq.com>
2020-12-22 12:58:45 +0800

    栈的压入弹出序列

---
+++ validateStackSequences.cpp
@@ -0,0 +1,28 @@
+
+/*
+	输入两个整数序列，第一个序列表示栈的压入顺序，请判断第二个序列是否为该栈的弹出顺序。
+	假设压入栈的所有数字均不相等。
+	例如，序列 {1,2,3,4,5} 是某栈的压栈序列，序列 {4,5,3,2,1} 是该压栈序列对应的一个弹出序列，
+	但 {4,3,5,1,2} 就不可能是该压栈序列的弹出序列。
+*/
+class Solution {
+public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+        stack<int> st;
+        int i = 0, j = 0;
+        while (j < popped.size())
+        {
+            while (st.empty() || st.top() != popped[j])
+            {
+                if (i < pushed.size())
+                    st.push(pushed[i++]);
+                else
+                    return false;
+            }
+
+            st.pop();
+            j++;
+        }
+        return true;
+    }
+};
